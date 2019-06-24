4625284e95b6b497f2e8ce66c7269d6bef6260b9
SilentProjects <psilentyt@gmail.com>
2019-06-24 01:45:29 -0500

    Update ProcMem.h

--- External Heckle/ProcMem.h
+++ External Heckle/ProcMem.h
@@ -6,13 +6,6 @@
 
 #define WIN32_LEAN_AND_MEAN //Excludes Headers We Wont Use (Increase Compile Time)
 
-#include <assert.h>
-#include <windows.h> //Standard Windows Functions/Data Types
-#include <iostream> //Constains Input/Output Functions (cin/cout etc..)
-#include <TlHelp32.h> //Contains Read/Write Functions
-#include <string> //Support For Strings
-#include <sstream> //Supports Data Conversion
-
 using namespace std;
 
 class ProcMem {
